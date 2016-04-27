7690a20c4086162b51ae5ca93bfe7e602396c81f
Selivanov Dmitry <diselivanov@gmail.com>
2016-04-27 21:31:50 +0300

    Use last update time to detect new entries

--- rss/updater.py
+++ rss/updater.py
@@ -1,23 +1,19 @@
 import time
 import feedparser
 import rss.sources as sources
-
-# in seconds
-MINUTE = 60
-HOUR = MINUTE * 60
-DAY = HOUR * 24
-WEEK = DAY * 7
+import data_storage
 
 # show only fresh entries
-is_entry_ok = lambda creation_time : abs(time.mktime(time.localtime()) - time.mktime(creation_time)) <= DAY
+is_new_entry = lambda creation_time : time.mktime(creation_time) >= data_storage.get_last_updated_time()
 
 def get_new_entries():
     new_entries = []
     for source in sources.rss_feeds:
-       new_entries += [{source['title'] : get_entries_since_yesterday(source['url'])}]
+       new_entries += [{source['title'] : get_entries_since_last_update(source['url'])}]
+    data_storage.save_last_updated_time()
     return new_entries
 
-def get_entries_since_yesterday(url):
+def get_entries_since_last_update(url):
     rss = feedparser.parse(url)
     if rss['status'] >= 200 and rss['status'] < 300 and len(rss['entries']) > 0:
-        return [entry for entry in rss['entries'] if is_entry_ok(entry['published_parsed'])]
+        return [entry for entry in rss['entries'] if is_new_entry(entry['published_parsed'])]
